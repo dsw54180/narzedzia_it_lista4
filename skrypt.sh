67a833bf5e0ed4929a60208e2edd3f92623fe43a
Jaru <jarek.surma@gmail.com>
2024-06-30 19:56:36 +0200

    poprawa --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
   date
-fi
-if [ "$1" == "--logs" ]; then
-  for i in {1..100}; do
-    echo "Numer pliku: $1, utworzono przez: skrpyt.sh, data: $(date)" > log_$1.txt
+elif [ "$1" == "--logs" ]; then
+  for ((i=1; i<=100; i++)); do
+    echo -e "Numer pliku: $i, stworzony przez: $0, data: $(date)" > log_$i.txt
   done
+else
+  echo "Unknown option: $1"
 fi
