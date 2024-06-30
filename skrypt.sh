298a274200b32ccdda1499694e31c5330f3992d0
Jaru <jarek.surma@gmail.com>
2024-06-30 20:17:14 +0200

    dodanie mozliwosci wpisania liczby plikow log

--- skrypt.sh
+++ skrypt.sh
@@ -2,9 +2,15 @@
 if [ "$1" == "--date" ]; then
   date
 elif [ "$1" == "--logs" ]; then
-  for ((i=1; i<=100; i++)); do
-    echo -e "Numer pliku: $i, stworzony przez: $0, data: $(date)" > log_$i.txt
-  done
+  if [ -n "$2" ]; then
+    for ((i=1; i<=$2; i++)); do
+      echo -e "Numer pliku: $i, stworzony przez: $0, data: $(date)" > log_$i.txt
+    done
+  else
+    for ((i=1; i<=100; i++)); do
+      echo -e "Numer pliku: $i, stworzony przez: $0, data: $(date)" > log_$i.txt
+    done
+  fi
 else
   echo "Unknown option: $1"
 fi
