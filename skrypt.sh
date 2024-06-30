673ec7e8682d005eeeaf127606f96150715368ee
Jaru <jarek.surma@gmail.com>
2024-06-30 19:27:11 +0200

    dodanie flagi --logs do skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,8 @@
 if [ "$1" == "--date" ]; then
   date
 fi
+if [ "$1" == "--logs" ]; then
+  for i in {1..100}; do
+    echo "Numer pliku: $1, utworzono przez: skrpyt.sh, data: $(date)" > log_$1.txt
+  done
+fi
